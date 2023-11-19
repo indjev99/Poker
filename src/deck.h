0353355fb4d4ecdb20192d50c1da443a6ee04897
Emil Indzhev <emil.indjev@gmail.com>
2023-11-19 02:21:18 +0100

    Update deck

--- src/deck.h
+++ src/deck.h
@@ -8,21 +8,34 @@
 struct Deck
 {
     int pos;
+    int maxSize;
     Card cards[NUM_CARDS];
 
     Deck()
     {
         pos = 0;
+        maxSize = NUM_CARDS;
         for (int id = 0; id < NUM_CARDS; ++id)
         {
             cards[id] = Card(id);
         }
     }
 
+    Deck(bool taken[NUM_CARDS])
+    {
+        pos = 0;
+        maxSize = 0;
+        for (int id = 0; id < NUM_CARDS; ++id)
+        {
+            if (taken[id]) continue;
+            cards[maxSize++] = Card(id);
+        }
+    }
+
     void shuffle(RNG& rng)
     {
         pos = 0;
-        std::shuffle(cards, cards + NUM_CARDS, rng.generator);
+        std::shuffle(cards, cards + maxSize, rng.generator);
     }
 
     constexpr Card drawCard()
@@ -32,6 +45,6 @@ struct Deck
 
     constexpr int size() const
     {
-        return NUM_CARDS - pos;
+        return maxSize - pos;
     }
 };
